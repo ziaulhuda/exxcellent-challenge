f2d4f1c053277ed3afc2872b8019d60959784ebc
Zia Ul-Huda <zia_ul_huda@yahoo.com>
2017-10-04 14:08:13 +0200

    Weather task done

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,18 +1,130 @@
 package de.exxcellent.challenge;
 
+import java.util.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets; 
+import java.nio.file.Files; 
+import java.nio.file.Path; 
+import java.nio.file.Paths;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
  *
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
+
+//structure to keep record of a single element in the whole colection
+class Record{
+	private HashMap<String, String> data; //HashMap to store data against each key from CSV file
+	
+	Record(){
+		data = new HashMap<String, String>();
+	}
+	
+	//create a record with map initialized
+	Record(String[] keys, String[] values){
+		data = new HashMap<String, String>();
+		for(int i=0; i < keys.length; ++i){
+			data.put(keys[i],values[i]);
+		}
+	}
+	
+	String getData(String key){
+		return data.get(key);
+	}
+	
+	int size(){
+		if(data == null)
+			return 0;
+		return data.size();
+	}
+}
+
+//class to handle whole data and operations on it.
+class AllRecords{
+	private ArrayList<Record> records;
+	
+	AllRecords(){
+		records = new ArrayList<Record>();
+	}
+	
+	int size(){
+		return records.size();
+	}
+	
+	Record getRecord(int x){
+		if(records == null || x >= records.size())
+			return null;
+		return records.get(x);
+	}
+	
+	//reads the input csv file and updates records list
+	void getInputData(String fileName){
+		Path pathToFile = Paths.get(fileName);
+		try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) {
+			// read the headers from the text file 
+			String line = br.readLine();
+			if(line.isEmpty()){
+				//invalid csv file.
+				System.out.println("Invalid file format");
+				System.exit(1);
+			}
+			
+			//get the attribute headers
+			String[] keys = line.split(",");
+			
+			line = br.readLine(); //read next line
+			
+			while(line != null){//iterate over whole file and add new record per line
+				String[] values = line.split(",");
+				
+				Record rec = new Record(keys, values);
+				records.add(rec);
+				line = br.readLine();
+			}
+
+		}catch (IOException ioe) {
+            ioe.printStackTrace();
+			System.exit(1);
+        }
+	}
+	
+	//Function to return the Record obj with smallest diff between the values of the two columns provided
+	Record recWithSmallestDifference(String first, String second){
+		
+		//initialize the smallest record with the first element of the records
+		Record smallestRec = records.get(0);
+		double smallestDiff = Math.abs(Double.parseDouble(records.get(0).getData(first)) - Double.parseDouble(records.get(0).getData(second)));
+		double temp;
+		
+		//iterate over wholel list
+		for(int i=1; i < records.size(); ++i){
+			temp = Math.abs(Double.parseDouble(records.get(i).getData(first)) - Double.parseDouble(records.get(i).getData(second)));
+			
+			//new minimum difference found
+			if (temp < smallestDiff){
+				smallestRec = records.get(i);
+				smallestDiff = temp;
+			}
+		}
+		
+		return smallestRec;
+	}
+}
+
 public final class App {
 
     public static void main(String... args) {
 
         // Your preparation code …
+		AllRecords weatherRecords = new AllRecords();
+		
+		//the input files can also be taken as input params to the program.
+		weatherRecords.getInputData(System.getProperty("user.dir") + "/target/classes/de/exxcellent/challenge/weather.csv");
+		Record smallestDiffDay = weatherRecords.recWithSmallestDifference("MxT","MnT");
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+        String dayWithSmallestTempSpread = smallestDiffDay.getData("Day");     // Your day analysis function call …
         String teamWithSmallesGoalSpread = "A good team"; // Your goal analysis function call …
 
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -3,6 +3,9 @@ package de.exxcellent.challenge;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
 
 /**
  * Example JUnit4 test case.
@@ -11,15 +14,57 @@ import org.junit.Test;
 public class AppTest {
 
     private String successLabel = "not successful";
+	private String weatherCSVPath = System.getProperty("user.dir") + "/target/test-classes/de/exxcellent/challenge/weather.csv";
+	private PrintWriter weatherFile;
+	
+	private void createFile(String fileName) throws IOException{
+		//create a sample weather test file with append mode = false
+		FileWriter write = new FileWriter( fileName , false);
+		weatherFile = new PrintWriter( write ); //FileWriter supports only bytes. PrintWriter supports text writing
+	}
+		
+	private void createWeatherCSV() throws IOException{
+		//create file
+		createFile(weatherCSVPath);
+		
+		//put test data into the file and close
+		String line = "Day,MxT,MnT,AvT,AvDP,1HrP TPcpn,PDir,AvSp,Dir,MxS,SkyC,MxR,Mn,R AvSLP";
+		weatherFile.printf("%s,%n", line);
+		line = "1,88,59,74,53.8,0,280,9.6,270,17,1.6,93,23,1004.5";
+		weatherFile.printf("%s,%n", line);			
+		line = "2,79,63,71,46.5,0,330,8.7,340,23,3.3,70,28,1004.5";
+		weatherFile.printf("%s,%n", line);
+		line = "3,77,55,66,39.6,0,350,5,350,9,2.8,59,24,1016.8";
+		weatherFile.printf("%s,%n", line);
+		
+		weatherFile.close();
+	}
 
     @Before
     public void setUp() throws Exception {
         successLabel = "successful";
+		//create test weather data
+		createWeatherCSV();
+		
+		
     }
+	
+	@Test
+	public void csvDataReading(){//test to check that data is read correctly
+		AllRecords weatherRecords = new AllRecords();
+		weatherRecords.getInputData(weatherCSVPath);
+		
+		Assert.assertEquals("All records read", 3, weatherRecords.size());
+		Record r = weatherRecords.getRecord(0);
+		Assert.assertEquals("Correct number of columns", 14, r.size());
+	}
 
     @Test
-    public void aPointlessTest() {
-        Assert.assertEquals("Expectations met", "successful", successLabel);
+    public void dayWithSmallestTempSpread() {
+		AllRecords weatherRecords = new AllRecords();
+		weatherRecords.getInputData(weatherCSVPath);
+		
+        Assert.assertEquals("Day with smallest temprature spread is correct", weatherRecords.getRecord(1), weatherRecords.recWithSmallestDifference("MxT","MnT"));
     }
 
 }
